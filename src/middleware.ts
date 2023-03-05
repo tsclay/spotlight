6dcf6e76ef5ea4a134eb8fb4f4bc01c92b19bae2
tsclay <tsclay9@gmail.com>
2023-03-05 00:46:16 -0500

    Add middleware to redirect authed users to main screen behind login

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server'
+import PocketBase from 'pocketbase';
+
+export async function middleware(req: NextRequest) {
+    if (req.nextUrl.pathname == '/') {
+        // console.log('has the pb auth cookie ', req.cookies.get('pb_auth')?.value)
+        const pb = new PocketBase('127.0.0.1:8090')
+        const pbCookie = `pb_auth=${encodeURIComponent(req.cookies.get('pb_auth')?.value ?? '')}`
+        console.log('the pbCookie ', pbCookie)
+        pb.authStore.loadFromCookie((pbCookie ?? '')) 
+        await pb.collection('users').authRefresh();
+        // pb.authStore.loadFromCookie(pbCookie ? `pb_auth=${pbCookie}`)
+        console.log('the pb valid check ', pb.authStore.isValid)
+        if (pb.authStore.isValid) {
+            console.log('should redirect')
+            const response = NextResponse.redirect(new URL('/snippets', req.url))
+            return response
+        }
+    }
+    // return NextResponse.next()
+}
+
+// export const config = {
+//     matcher: ['/(?!auth).*'],
+//   }
