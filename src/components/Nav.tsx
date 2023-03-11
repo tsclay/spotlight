eb59debd4c7a48de44cda29ed635c7e69db6e4ea
tsclay <tsclay9@gmail.com>
2023-03-11 01:12:52 -0500

    Restyle nav to be a toggleable aside

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,38 @@
-import Link from "next/link"
+import Link from "next/link";
+import { SyntheticEvent } from "react";
 
 export default function Nav() {
-    const links = [
-        {
-            href: '/snippets',
-            name: 'Snippets'
-        },
-        {
-            href: '/you',
-            name: 'Account'
-        }
-    ]
-    return (
-        <nav className="bg-blue-300 p-4 flex justify-end gap-1">
-            {links.map(l => (
-                <Link key={l.href} href={l.href}>{l.name}</Link>
-            ))}
-        </nav>
-    )
+  const links = [
+    {
+      href: "/snippets",
+      name: "Snippets",
+    },
+    {
+      href: "/you",
+      name: "Account",
+    },
+  ];
+
+  const handleToggleAside = (e: SyntheticEvent) => {
+    const aside = (e.currentTarget as HTMLButtonElement).nextElementSibling;
+    aside?.classList.toggle("nav-show");
+  }
+
+  return (
+    <>
+      <button
+        className="fixed bottom-1 left-1 z-50 text-xl bg-blue-300 border-black rounded-full p-2"
+        onClick={handleToggleAside}
+      >
+        ⬅️
+      </button>
+      <aside className="nav-hide fixed left-0 z-40 flex h-full flex-col justify-start gap-1 bg-blue-300 p-4">
+        {links.map((l) => (
+          <Link key={l.href} href={l.href}>
+            {l.name}
+          </Link>
+        ))}
+      </aside>
+    </>
+  );
 }
