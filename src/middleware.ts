552777fbb8f8c893e78ad51166a57febc565ddfc
tsclay <tsclay9@gmail.com>
2023-03-11 01:12:30 -0500

    Fix URL for PocketBase instance

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import PocketBase from 'pocketbase';
 export async function middleware(req: NextRequest) {
     if (req.nextUrl.pathname == '/') {
         // console.log('has the pb auth cookie ', req.cookies.get('pb_auth')?.value)
-        const pb = new PocketBase('127.0.0.1:8090')
+        const pb = new PocketBase('http://127.0.0.1:8090')
         const pbCookie = `pb_auth=${encodeURIComponent(req.cookies.get('pb_auth')?.value ?? '')}`
         console.log('the pbCookie ', pbCookie)
         pb.authStore.loadFromCookie((pbCookie ?? '')) 
