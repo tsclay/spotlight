e34c5dacc45e3370ef235b2ca7ca4db19b35992b
tsclay <tsclay9@gmail.com>
2023-06-05 21:44:50 -0400

    Guard middleware run & handle unauth'd user

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,31 @@ import { NextRequest, NextResponse } from 'next/server'
 import PocketBase from 'pocketbase';
 
 export async function middleware(req: NextRequest) {
-    if (req.nextUrl.pathname == '/') {
+    // if (req.nextUrl.pathname == '/') {
         // console.log('has the pb auth cookie ', req.cookies.get('pb_auth')?.value)
         const pb = new PocketBase('http://127.0.0.1:8090')
         const pbCookie = `pb_auth=${encodeURIComponent(req.cookies.get('pb_auth')?.value ?? '')}`
         console.log('the pbCookie ', pbCookie)
         pb.authStore.loadFromCookie((pbCookie ?? '')) 
-        await pb.collection('users').authRefresh();
+
+        try {
+            // get an up-to-date auth store state by veryfing and refreshing the loaded auth model (if any)
+            if (!pb.authStore.isValid) {
+                throw Error("blah")
+            }
+            await pb.collection('users').authRefresh();
+            console.log('the pb valid check ', pb.authStore.isValid)
+            console.log('should redirect')
+            // const response = NextResponse.redirect(new URL('/snippets', req.url))
+            const response = NextResponse.next()
+            return response
+        } catch (_) {
+            // clear the auth store on failed refresh
+            console.log('catch block')
+            pb.authStore.clear();
+            return NextResponse.redirect(new URL('/auth/login', req.url))
+        }
+        // await pb.collection('users').authRefresh();
         // pb.authStore.loadFromCookie(pbCookie ? `pb_auth=${pbCookie}`)
         console.log('the pb valid check ', pb.authStore.isValid)
         if (pb.authStore.isValid) {
@@ -16,10 +34,10 @@ export async function middleware(req: NextRequest) {
             const response = NextResponse.redirect(new URL('/snippets', req.url))
             return response
         }
-    }
+    // }
     // return NextResponse.next()
 }
 
-// export const config = {
-//     matcher: ['/(?!auth).*'],
-//   }
+export const config = {
+    matcher: ['/snippets/:path*', '/you/:path*'],
+  }
