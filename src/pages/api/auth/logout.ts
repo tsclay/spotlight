028725ac25cd26679c12efb35aca30289dfc7aaf
tsclay <tsclay9@gmail.com>
2023-09-05 08:08:20 -0400

    Handle logout and redirect

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -13,7 +13,8 @@ export default async function handler(
     try {
         const pb = await initPocketBase(req, res);
         pb.authStore.clear();
-        return res.status(200).json({message: 'Success'})
+        // return res.status(200).json({message: 'Success'})
+    return res.redirect(302, '/')
       } catch (error) {
         console.log(error)
         return res.status(400).json({message: 'Something went wrong while creating record.'})
